eb49385148572e7e66bf16c59beef0e9f71db6f6
ilyanosovsky <imnosovsky@gmail.com>
2023-08-06 14:27:53 +0300

    try to fix bug with adding friends

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -56,7 +56,7 @@ export const addRemoveFriend = async (req, res) => {
             }
         );
         res.status(200).json(formattedFriends);
-
+        
     } catch (err) {
         res.status(404).json({ message: err.message });
     }
