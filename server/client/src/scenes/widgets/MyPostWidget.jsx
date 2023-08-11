7393afda03405b520b7c41491dde33b608c91d18
ilyanosovsky <imnosovsky@gmail.com>
2023-08-11 19:29:47 +0300

    map is working, but not marker

--- server/client/src/scenes/widgets/MyPostWidget.jsx
+++ server/client/src/scenes/widgets/MyPostWidget.jsx
@@ -1,146 +1,216 @@
 import {
-    EditOutlined,
-    AddLocationOutlined,
-    EventOutlined,
-  } from "@mui/icons-material";
-  import {
-    Box,
-    Divider,
-    Typography,
-    InputBase,
-    useTheme,
-    Button,
-    IconButton,
-    useMediaQuery,
-    MenuItem,
-    Select,
-    FormControl,
-    InputLabel,
-    TextField,
-  } from "@mui/material";
-  import FlexBetween from "components/FlexBetween";
-  import UserImage from "components/UserImage";
-  import WidgetWrapper from "components/WidgetWrapper";
-  import { useState, useEffect } from "react";
-  import { useDispatch, useSelector } from "react-redux";
-  import { setPosts } from "state";
-  import { fetchCourts } from "api/courts";
-  
-  const MyPostWidget = ({ picturePath }) => {
-    const dispatch = useDispatch();
-    const [post, setPost] = useState(""); // post content 
-    const [selectedCourt, setSelectedCourt] = useState(""); // selected court
-    const [selectedDate, setSelectedDate] = useState(null); // State for selected date and time
-    const [courts, setCourts] = useState([]); // list of courts
-    const { palette } = useTheme(); // our colors
-    const { _id, firstName, lastName, level } = useSelector((state) => state.user); // send it to the Back
-    const token = useSelector((state) => state.token);
-    // const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-    // const mediumMain = palette.neutral.mediumMain;
-    // const medium = palette.neutral.medium;
+  EditOutlined,
+  AddLocationOutlined,
+  EventOutlined
+} from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  Typography,
+  InputBase,
+  useTheme,
+  Button,
+  IconButton,
+  useMediaQuery,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  TextField,
+} from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import UserImage from "components/UserImage";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import { fetchCourts } from "api/courts";
 
-      // Fetch the list of courts when the component mounts
-    useEffect(() => {
-      async function fetchCourtsData() {
-        try {
-          const courtsData = await fetchCourts();
-          setCourts(courtsData);
-        } catch (error) {
-          console.error("Error fetching courts:", error);
-        }
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+  
+const MyPostWidget = ({ picturePath }) => {
+  const dispatch = useDispatch();
+  const [post, setPost] = useState(""); // post content 
+  const [selectedCourt, setSelectedCourt] = useState(""); // selected court
+  const [selectedDate, setSelectedDate] = useState(null); // State for selected date and time
+  const [courts, setCourts] = useState([]); // list of courts
+  const { palette } = useTheme(); // our colors
+  const { _id } = useSelector((state) => state.user); // send it to the Back
+  const token = useSelector((state) => state.token);
+  const [showMap, setShowMap] = useState(false); // map to show
+  const [selectedCourtOnMap, setSelectedCourtOnMap] = useState(null); //selected courts on map
+  // const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  // const mediumMain = palette.neutral.mediumMain;
+  // const medium = palette.neutral.medium;
+
+    // Fetch the list of courts when the component mounts
+  useEffect(() => {
+    async function fetchCourtsData() {
+      try {
+        const courtsData = await fetchCourts();
+        setCourts(courtsData);
+      } catch (error) {
+        console.error("Error fetching courts:", error);
       }
+    }
 
-      fetchCourtsData();
-    }, []);
+    fetchCourtsData();
+  }, []);
+
+
+  // map set up
+  const toggleMap = () => {
+    setShowMap(!showMap);
+  };
+
+  const handleMarkerClick = (court) => {
+    setSelectedCourtOnMap(court);
+    setSelectedCourt(court._id); // Automatically select the court in the dropdown
+    // toggleMap(); // Close the map after selecting a court
+  };
+
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append("userId", _id);
+    formData.append("courtId", selectedCourt);
+    formData.append("description", post);
+    formData.append("dateAndTime", selectedDate); // Append selected date and time to the form data
+
+    const response = await fetch(`http://localhost:3001/posts`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+      body: formData,
+    });
+    const posts = await response.json();
+    dispatch(setPosts({ posts }));
+    setPost("");
+  };
+
+  console.log("my post: courts data ->", courts);
+  // Find the selected court based on the selectedCourt value
+  const selectedCourtInfo = courts.find(court => court._id === selectedCourt);
   
-    const handlePost = async () => {
-      const formData = new FormData();
-      formData.append("userId", _id);
-      formData.append("courtId", selectedCourt);
-      formData.append("description", post);
-      formData.append("dateAndTime", selectedDate); // Append selected date and time to the form data
-  
-      const response = await fetch(`http://localhost:3001/posts`, {
-        method: "POST",
-        headers: { Authorization: `Bearer ${token}` },
-        body: formData,
-      });
-      const posts = await response.json();
-      dispatch(setPosts({ posts }));
-      setPost("");
-    };
-  
-    return (
-      <WidgetWrapper>
-        <FlexBetween gap="1.5rem">
-          <UserImage image={picturePath} />
-          <InputBase
-            placeholder="Description of the Match..."
-            onChange={(e) => setPost(e.target.value)}
-            value={post}
-            sx={{
-              width: "100%",
-              backgroundColor: palette.neutral.light,
-              borderRadius: "2rem",
-              padding: "1rem 2rem",
+  return (
+    <WidgetWrapper>
+      <FlexBetween gap="1.5rem">
+        <UserImage image={picturePath} />
+        <InputBase
+          placeholder="Description of the Match..."
+          onChange={(e) => setPost(e.target.value)}
+          value={post}
+          sx={{
+            width: "100%",
+            backgroundColor: palette.neutral.light,
+            borderRadius: "2rem",
+            padding: "1rem 2rem",
+          }}
+        />
+      </FlexBetween>
+
+      <Divider sx={{ margin: "1.25rem 0" }} />
+
+      {/* Map Toggle Button */}
+
+      <Box sx={{padding: "1rem 0rem"}}>
+        <Button onClick={toggleMap}>
+          {showMap ? "Hide Map" : "Show Map"}
+        </Button>
+      {/* Leaflet Map */}
+      {showMap && (
+        <MapContainer
+          center={[32.794044, 34.989571]} // Set the initial map center coordinates
+          zoom={13} // Set the initial zoom level
+          style={{ width: "100%", height: "400px", marginTop: "1rem" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          {/* Render markers for each court */}
+          {courts.map((court) => (
+            <Marker
+              key={court._id}
+              position={[court.latitude, court.longitude]} // Use the actual coordinates of the court
+              eventHandlers={{
+                click: () => handleMarkerClick(court),
+              }}
+            >
+              <Popup>{court.courtName}</Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      )}
+
+      </Box>
+
+      <FlexBetween>
+        <FormControl fullWidth variant="outlined" sx={{ marginBottom: "1rem" }}>
+          <InputLabel>Select a Court</InputLabel>
+          <Select
+            value={selectedCourt}
+            onChange={(e) => setSelectedCourt(e.target.value)}
+            label="Select a Court"
+          >
+            {courts.map((court) => (
+              <MenuItem key={court._id} value={court._id}>
+                {court.courtName} court; location: {court.location}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </FlexBetween>
+
+      <FlexBetween>
+      {/* Date and Time Picker */}
+        <TextField
+            id="dateAndTime"
+            label="Date and Time"
+            type="datetime-local"
+            value={selectedDate}
+            onChange={(e) => setSelectedDate(e.target.value)}
+            InputLabelProps={{
+              shrink: true,
             }}
           />
-        </FlexBetween>
-  
-        <Divider sx={{ margin: "1.25rem 0" }} />
 
-        <FlexBetween>
-          <FormControl fullWidth variant="outlined" sx={{ marginBottom: "1rem" }}>
-            <InputLabel>Select a Court</InputLabel>
-            <Select
-              value={selectedCourt}
-              onChange={(e) => setSelectedCourt(e.target.value)}
-              label="Select a Court"
-            >
-              {courts.map((court) => (
-                <MenuItem key={court._id} value={court._id}>
-                  {court.courtName} court; location: {court.location}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </FlexBetween>
+          {/* Display selected court information */}
+          {selectedCourtInfo && (
+            <Box 
+            sx={{ 
+              padding: "1rem 2rem" 
+              }}>
+              <Typography variant="subtitle2">Information about court:</Typography>
+              <Typography>Ground type: {selectedCourtInfo.ground}</Typography>
+              <Typography>Facilities: {selectedCourtInfo.facilities.join(", ")}</Typography>
+              <Typography>Working Hours: from {selectedCourtInfo.startTime} till {selectedCourtInfo.endTime}</Typography>
+            </Box>
+          )}
+      </FlexBetween>
 
-        <FlexBetween>
-        {/* Date and Time Picker */}
-          <TextField
-              id="dateAndTime"
-              label="Date and Time"
-              type="datetime-local"
-              value={selectedDate}
-              onChange={(e) => setSelectedDate(e.target.value)}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-            <Box>
-              <Typography>{selectedCourt}</Typography>              
-            </Box>
-        </FlexBetween>
-  
-        <FlexBetween> 
-            <Box>
-       
-            </Box>
-          <Button
-            disabled={!post || !selectedCourt}
-            onClick={handlePost}
-            sx={{
-              color: palette.background.alt,
-              backgroundColor: palette.primary.main,
-              borderRadius: "3rem",
-            }}
-          >
-            Create a Match
-          </Button>
-        </FlexBetween>
-      </WidgetWrapper>
-    );
-  };
-  
-  export default MyPostWidget;
+      <FlexBetween> 
+          <Box>
+      
+          </Box>
+        <Button
+          disabled={!post || !selectedCourt}
+          onClick={handlePost}
+          sx={{
+            color: palette.background.alt,
+            backgroundColor: palette.primary.main,
+            borderRadius: "3rem",
+          }}
+        >
+          Create a Match
+        </Button>
+      </FlexBetween>
+    </WidgetWrapper>
+  );
+};
+
+export default MyPostWidget;
